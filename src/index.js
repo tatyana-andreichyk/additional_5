a5a3578fe438910c059158090e8fc148e979dd52
tatyana-andreichyk <tatyana.andreichyk@gmail.com>
2018-09-26 20:37:10 +0300

    additional_5/feature: implemented function for brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let openBrackets = {};
+    
+    for (let i = 0; i < bracketsConfig.length; i++){
+        openBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    
+    let stack = [];
+    
+    let arrayElemStr = str.split("");
+
+    for (let j = 0; j < arrayElemStr.length; j++){
+ 
+		let currentElemInStack = stack.pop();
+        let closingPairElementInStack = openBrackets[currentElemInStack];
+        
+       if (openBrackets[arrayElemStr[j]] && (arrayElemStr[j] !== currentElemInStack)) {
+		   stack.push(currentElemInStack);
+           stack.push(arrayElemStr[j]);
+       } else {
+           if (arrayElemStr[j] !== closingPairElementInStack){
+               return false;               
+           }
+       }  
+    }
+    
+
+    if (stack.pop()){
+        return false;
+    }else{
+		return true;
+	}
+    
 }
